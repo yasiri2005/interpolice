787a1163a7fc41b4f5b1b7234a9fef19e8af1eb7
Razieljk <juansexd7w7@gmail.com>
2026-02-27 07:56:28 -0500

    render

--- BACK/modules/bdatos.js
+++ BACK/modules/bdatos.js
@@ -1,9 +1,30 @@
 //!modulo para conexion a la base de datos
 import mysql from "mysql2";
-let cnx;
+
+
+const conexion = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    
+});
+
+
+conexion.connect((error)=> {
+    if(error){
+        console.log(error);
+    }else{
+        console.log("connection successful");
+    }
+});
+
+
+/*
+//let cnx;
 try {
-   cnx = mysql.createConnection({
-  host: "bunozzsosbgspwq9dm1j-mysql.services.clever-cloud.com-mysql.services.clever-cloud.com",
+  cnx = mysql.createConnection({
+  //host: "bunozzsosbgspwq9dm1j-mysql.services.clever-cloud.com-mysql.services.clever-cloud.com",
   user: "umsb4s95ss1lilpr",
   database: "bunozzsosbgspwq9dm1j",
   // port:"3306",
@@ -12,4 +33,4 @@ try {
 } catch (error) {
     console.log(`Ha ocurrido un error en la conexion: ${error.message}`);
 }
-export { cnx };
+export { cnx };*/
